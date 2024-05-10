5fe41351ac7a646849514c0bb2b8faca8d013b6f
Mohamed Elsayed <65646207+modi161@users.noreply.github.com>
2024-05-10 22:07:11 +0300

    now delete post is fixed

--- test/DBtest.py
+++ test/DBtest.py
@@ -10,14 +10,10 @@ my_db = pymysql.connect(
 
 my_cursor = my_db.cursor()
 
-# my_cursor.execute("SHOW TABLES")
-
-#my_cursor.execute(f'UPDATE Family SET profilephoto = "https://www.aupair.com/summaryBoxImages/au-pair-wiki-photos-2632.jpg", coverphoto = "https://images.pexels.com/photos/39691/family-pier-man-woman-39691.jpeg" WHERE id = {5};')
-#my_db.commit()
 
 
-
-my_cursor.execute("Select * from contentphotos")
+my_cursor.execute("SHOW TABLES")
+# my_cursor.execute("Select * from ")
 
 
 
